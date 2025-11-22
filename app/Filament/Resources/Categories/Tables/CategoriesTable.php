f1278700292d13433e94c2f90f52a28a5eabcb24
yunanfs <yunanpkl1@gmail.com>
2025-11-22 14:04:34 +0700

    membuat categories

---
+++ app/Filament/Resources/Categories/Tables/CategoriesTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+
+class CategoriesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->label('Name'),
+                TextColumn::make('description')->label('Description'),
+            ])
+            ->filters([
+                SelectFilter::make('name')
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
